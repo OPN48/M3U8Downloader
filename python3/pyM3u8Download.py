4936dcf06174e0edb0f223e7e32e7a9448377ad5
luke <cuba3@163.com>
2021-04-07 11:31:29 +0800

    新增request header兼容部分网站修复404错误，新增一层无m3u8后缀名兼容，新增AES加密key绝对地址兼容，新增downloadBaseNameList合并兼容，部分请求改用post增加稳定性

--- python3/pyM3u8Download.py
+++ python3/pyM3u8Download.py
@@ -1,10 +1,12 @@
-import time , os
+import os
 import queue
 import threading
+import time
 
 # 下载最大重试次数
-repeatMax=5
-repeatWaitTime=30
+repeatMax = 5
+repeatWaitTime = 30
+# header = ''
 
 # 正则模块校验
 try:
@@ -36,6 +38,7 @@ except:
 queueLock = threading.Lock()
 workQueue = queue.Queue()
 
+
 def getSysArgv(keyReplace, safeKeys):
     inputDic = {}
     import sys
@@ -61,7 +64,7 @@ def getSysArgv(keyReplace, safeKeys):
 
 
 class myThread(threading.Thread):
-    def __init__(self, threadID, threadName,type='download',key=''):
+    def __init__(self, threadID, threadName, type='download', key=''):
         self.exitFlag = 0
         self.timeoutError = 0
         self.type = type
@@ -70,14 +73,15 @@ class myThread(threading.Thread):
         self.threadID = threadID
         self.threadName = threadName
         self.key = key
-        self.missionList=[]
+        self.missionList = []
+
     def download(self, threadName):
         while not self.exitFlag:
             queueLock.acquire()  # 保持线程同步
             if not workQueue.empty():
                 m = workQueue.get()
-                pdUrl= m[0]
-                tsTime= m[1]
+                pdUrl = m[0]
+                tsTime = m[1]
                 missStr = m[2]
                 queueLock.release()
                 # 获取文件名，方便映射m3u8
@@ -87,9 +91,9 @@ class myThread(threading.Thread):
                 # print(pdUrl,fuleName)
                 # time.sleep(3)
                 if fuleName not in os.listdir(self.download_path):
-                    resCount=0
-                    print('%s%s准备下载%s 保存为 %s' % (missStr,threadName, pdUrl, fuleName))
-                    while resCount<=repeatMax:
+                    resCount = 0
+                    print('%s%s准备下载%s 保存为 %s' % (missStr, threadName, pdUrl, fuleName))
+                    while resCount <= repeatMax:
                         try:
                             # 修改request模式为 url + params ,支持超长request
                             p = urllib.parse.urlparse(pdUrl)
@@ -97,18 +101,23 @@ class myThread(threading.Thread):
                             pTemp = urllib.parse.parse_qs(p.query)
                             for i in pTemp:
                                 pTemp[i] = pTemp[i][0]
-                            res = requests.get(url=u,params=pTemp)
-                            resC=res.content
+                            res = requests.get(url=u, params=pTemp)
+                            # res = requests.post(url=u, params=pTemp)
+                            resC = res.content
                             break
                         except:
                             time.sleep(repeatWaitTime)
-                            resCount+=1
-                            resC=''
+                            resCount += 1
+                            resC = ''
                     if len(self.key):  # AES 解密
+                        # print("================")
+                        # print(self.key, AES.MODE_CBC, self.key)
                         cryptor = AES.new(self.key, AES.MODE_CBC, self.key)
-                        with open(os.path.join(self.download_path, fuleName + ".mp4"), 'ab') as f:
+                        # if '.' not in fuleName:
+                            # fuleName += '.ts'
+                        with open(os.path.join(self.download_path, fuleName), 'ab') as f:
                             f.write(cryptor.decrypt(resC))
-                            result = (os.path.join(self.download_path, fuleName + ".mp4"), tsTime)
+                            result = (os.path.join(self.download_path, fuleName), tsTime)
                     else:
                         with open(os.path.join(self.download_path, fuleName), 'ab') as f:
                             f.write(resC)
@@ -119,6 +128,7 @@ class myThread(threading.Thread):
             else:
                 queueLock.release()
         return result
+
     def run(self):
         print("开启线程：" + self.threadName)
         if self.type == 'download':
@@ -126,8 +136,9 @@ class myThread(threading.Thread):
         # 此处可以扩展根据业务不同执行不同的多线程，目前仅使用下载
         print("退出线程：" + self.threadName)
 
-def doMission(missionList=[],threadNum=5,type='download',key=''):
-    if missionList!=[]:
+
+def doMission(missionList=[], threadNum=5, type='download', key=''):
+    if missionList != []:
         threads = []
         threadID = 1
         # 创建新线程
@@ -151,36 +162,44 @@ def doMission(missionList=[],threadNum=5,type='download',key=''):
         # 等待所有线程完成
         for t in threads:
             # 通知线程是时候退出
-            t.exitFlag=1
+            t.exitFlag = 1
             t.join(timeout=180)
         print("退出主线程")
+
 
 class M3u8PParsing():
     def __init__(self):
         self.dlPath = './downloads/'
         self.tsDic = {}
-        self.stepTsDic={}
+        self.stepTsDic = {}
         self.key = ''
-        self.info=''
-        self.jpgList=[]
-        self.finishImgList=[]
+        self.info = ''
+        self.downloadBaseNameList = []
         self.threadNum = 5
-    def merge2Mp4(self, path,name='new'):
-        filesList=os.listdir(path)
-        filesList.sort()
+
+    def merge2Mp4(self, path, name='new'):
+        if self.downloadBaseNameList:
+            # 用于全流程直接使用
+            filesList = self.downloadBaseNameList
+        else:
+            # 用于外部调用指定文件夹
+            filesList = os.listdir(path)
+            filesList.sort()
         os.chdir(path)
         files = ''
         # 部分下载文件可在此判断是会否成功，比如小于1KB忽略后合并
 
         for i in filesList:
-            files+=i+' '
-        cmd = 'cat '+files+'> '+name+'.tmp'
+            files += i + ' '
+        cmd = 'cat ' + files + '> ' + name + '.tmp'
         os.system(cmd)
         os.system('rm -rf *.ts')
         os.system('rm -rf *.mp4')
-        os.rename(name+".tmp", name+".mp4")
-    def getTsdic(self,url):
+        os.rename(name + ".tmp", name + ".mp4")
+
+    def getTsdic(self, url):
         host = url[:url.find('/', url.find('//') + 2)]
+        # all_content = requests.get(url).text  # 获取第一层M3U8文件内容
         all_content = requests.get(url).text  # 获取第一层M3U8文件内容
         if "#EXTM3U" not in all_content:
             raise BaseException("非M3U8的链接")
@@ -188,22 +207,31 @@ class M3u8PParsing():
             fileLine = all_content.split("\n")
             for line in fileLine:
                 if 'EXT-X-STREAM-INF' in line:
-                    self.info=re.findall(r'EXT-X-STREAM-INF:(.*?)$', line)[0]
-                if '.m3u8' in line:
-                    if line[:1]=='/':
-                        if line[:4]=='http':
-                            url=line
+                    self.info = re.findall(r'EXT-X-STREAM-INF:(.*?)$', line)[0]
+                elif '.m3u8' in line:
+                    if line[:1] == '/':
+                        if line[:4] == 'http':
+                            url = line
                         else:
-                            url=host+line
+                            url = host + line
                     else:
                         url = url.rsplit("/", 1)[0] + "/" + line  # 拼出第二层m3u8的URL
-                    # print(url)
-                    all_content = requests.get(url).text
+                    # print('EXT-X-STREAM-INF', url)
+                    all_content = requests.post(url).text
+                elif '#' not in line and line.strip():
+                    url = host + line.strip()
+                    # print('======else # not in line and line.strip()======', url)
+                    all_content = requests.post(url).text
+                    # print(all_content)
+                else:
+                    pass
         fileLine = all_content.split("\n")
         unknow = True
-        t=0.0
+        t = 0.0
+        # print(fileLine)
         for index, line in enumerate(fileLine):  # 第二层
             if "#EXT-X-KEY" in line:  # 找解密Key
+                unknow = False
                 methodPos = line.find("METHOD")
                 commaPoS = line.find(",")
                 method = line[methodPos:commaPoS].split('=')[1]
@@ -211,52 +239,62 @@ class M3u8PParsing():
                 uri_pos = line.find("URI")
                 quotation_mark_pos = line.rfind('"')
                 key_path = line[uri_pos:quotation_mark_pos].split('"')[1]
-                if key_path[:1]=='/':
-                    key_url=host+key_path
+                # print('======key_path=======',key_path)
+                if key_path[:4] == 'http':
+                    key_url = key_path
+                elif key_path[:1] == '/':
+                    key_url = host + key_path
                 else:
                     key_url = url.rsplit("/", 1)[0] + "/" + key_path  # 拼出key解密密钥URL
                 res = requests.get(key_url)
+                if '404' in str(res.content):
+                    print('======获取key失败======',key_url,res.content)
                 self.key = res.content
-                # print("key：", self.key)
+                # print("======self.key======", self.key)
             if "EXTINF" in line:  # 找ts地址并下载
                 unknow = False
                 # ts时长
                 length = float(re.findall(r'EXTINF:(.*?),', line.strip())[0])
                 # 进度时长time
-                beginTime=round(t,3)
+                beginTime = round(t, 3)
                 t += length
                 # url拼接模块
-                if fileLine[index + 1][:1]=='/':
-                    pdUrl=host+fileLine[index + 1]
+                if fileLine[index + 1][:1] == '/':
+                    pdUrl = host + fileLine[index + 1]
                 elif '://' in fileLine[index + 1]:
                     pdUrl = fileLine[index + 1]
                 else:
                     pdUrl = url.rsplit("/", 1)[0] + "/" + fileLine[index + 1]  # 拼出ts片段的URL
-                #去除url回车
+                # 去除url回车
                 pdUrl = pdUrl.strip()
-                self.tsDic[beginTime]={'url':pdUrl,'length':length,'file':fileLine[index + 1]}
+                self.tsDic[beginTime] = {'url': pdUrl, 'length': length, 'file': fileLine[index + 1]}
         if unknow:
             raise BaseException("未找到对应的下载链接")
         else:
             print("下载字典输出完成")
+        print("对字典进行排序生成downloadBaseNameList用于兼容不同系统文件排序")
+        for beginTime in sorted(self.tsDic.keys()):
+            self.downloadBaseNameList.append(os.path.basename(self.tsDic[beginTime]['file']))
         return self.tsDic
-    def getStepDic(self,step):
+
+    def getStepDic(self, step):
         lasttime = 0.0
-        if step==0:
-            self.stepTsDic=self.tsDic
+        if step == 0:
+            self.stepTsDic = self.tsDic
         else:
             for i in self.tsDic:
-                beginTime=i
-                time=round(beginTime+self.tsDic[i]['length'])
+                beginTime = i
+                time = round(beginTime + self.tsDic[i]['length'])
                 if (time - lasttime) >= step:
-                    self.stepTsDic[i]=self.tsDic[i]
+                    self.stepTsDic[i] = self.tsDic[i]
                     lasttime = time
         return self.stepTsDic
-    def downloadM3u8(self,missionList):
-        if missionList!=[]:
+
+    def downloadM3u8(self, missionList):
+        if missionList != []:
             if not os.path.exists(self.dlPath):
                 os.makedirs(self.dlPath)
-            finishTsList=os.listdir(self.dlPath)
+            finishTsList = os.listdir(self.dlPath)
             missionDownloadList = []
             for j in missionList:
                 if j[0].rsplit("/", 1)[-1] not in finishTsList:
@@ -270,18 +308,20 @@ class M3u8PParsing():
             return tempList
         else:
             return []
+
     def justDownload(self):
         missionDic = self.tsDic
         missionList = []
-        missionNum=1
+        missionNum = 1
         for i in missionDic:
             beginTime = i
             pdUrl = missionDic[i]['url']
             tsTime = str(beginTime)
-            missStr='(%s/%s)'%(missionNum,len(missionDic))
-            missionList.append((pdUrl,tsTime,missStr))
-            missionNum+=1
+            missStr = '(%s/%s)' % (missionNum, len(missionDic))
+            missionList.append((pdUrl, tsTime, missStr))
+            missionNum += 1
         self.downloadM3u8(missionList)
+
 
 if __name__ == '__main__':
     modelDic = {
@@ -300,15 +340,15 @@ if __name__ == '__main__':
         print(helpStr)
     else:
         # 指令合并
-        mDic={}
+        mDic = {}
         for i in modelDic:
-            mDic[i]=modelDic[i][2]
+            mDic[i] = modelDic[i][2]
         for i in inputDic:
-            mDic[i]=inputDic[i]
+            mDic[i] = inputDic[i]
         print(mDic)
         url = mDic['url']
         x = M3u8PParsing()
-        x.dlPath=mDic['dlpath']
+        x.dlPath = mDic['dlpath']
         x.getTsdic(url)
         x.justDownload()
         if mDic['type'] == 'mp4':
